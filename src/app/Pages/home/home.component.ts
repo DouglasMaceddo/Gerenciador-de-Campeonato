64c4cfb469b2a49fc6347e73663802790bd2e26c
DouglasMaceddo <dougasdv@gmail.com>
2025-02-12 11:22:10 -0300

    commit

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../Services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  constructor (private router: Router){}
+  constructor(private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    const user = this.authService.getUser();
+    if (user) {
+      this.router.navigate(['Perfil']);
+    }
+  }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
