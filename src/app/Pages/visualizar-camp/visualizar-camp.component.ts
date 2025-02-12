3ac098c8ca52573439d6627ef346985dc52c9451
DouglasMaceddo <dougasdv@gmail.com>
2025-02-12 11:23:18 -0300

    carregando campeonatos

--- src/app/Pages/visualizar-camp/visualizar-camp.component.ts
+++ src/app/Pages/visualizar-camp/visualizar-camp.component.ts
@@ -1,25 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Campeonato } from '../../Models/camp.model';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-visualizar-camp',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './visualizar-camp.component.html',
-  styleUrl: './visualizar-camp.component.css'
+  styleUrls: ['./visualizar-camp.component.css']
 })
-export class VisualizarCampComponent {
+export class VisualizarCampComponent implements OnInit {
+  campeonato: Campeonato | undefined;
+  campeonatoId: string = '';
 
-  campeonatos: any [] = [];
-
-  constructor(private router: Router){
-  }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.campeonatos = JSON.parse(localStorage.getItem('campeonatos') || '[]');
+    this.campeonatoId = this.route.snapshot.paramMap.get('id') || '';
+    this.carregarCampeonato();
   }
 
-  voltarHome(router: string){
-    this.router.navigate([router]);
-}
+  carregarCampeonato(): void {
+    const campeonatos = JSON.parse(localStorage.getItem('campeonatos') || '[]');
+    this.campeonato = campeonatos.find((camp: Campeonato) => camp.id === this.campeonatoId);
+    if (!this.campeonato) {
+      console.error('Campeonato não encontrado!');
+    }
+  }
+
+  marcarJogos(): void {
+    if (this.campeonato && this.campeonato.times) {
+      const jogos = [];
+      for (let i = 0; i < this.campeonato.times.length; i++) {
+        for (let j = i + 1; j < this.campeonato.times.length; j++) {
+          jogos.push({
+            time1: this.campeonato.times[i],
+            time2: this.campeonato.times[j],
+            data: null // Aqui você pode adicionar um campo de data
+          });
+        }
+      }
+      console.log(jogos);  // Exibindo os jogos no console para teste
+    }
+  }
+
+  voltarHome() {
+    this.router.navigate(['/Home']);
+  }
 }
