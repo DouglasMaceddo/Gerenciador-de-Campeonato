3765e6c756240fd6dfa1d19684f25b8c7ba23225
DouglasMaceddo <dougasdv@gmail.com>
2025-02-12 11:22:42 -0300

    adicionando perfil ao usuario

--- src/app/Pages/perfil/perfil.component.ts
+++ src/app/Pages/perfil/perfil.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../Services/auth.service';
 import { CommonModule } from '@angular/common';
+import { Campeonato } from '../../Models/camp.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -8,13 +10,36 @@ import { CommonModule } from '@angular/common';
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+  campeonatos: Campeonato[] = [];
+  userId: string = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
 
-  constructor(private authService: AuthService){}
-
-
-  logout(){
+  ngOnInit(): void {
+    const user = this.authService.getUser();
+    if (user && user.uid) {
+      this.userId = user.uid;
+      this.carregarCampeonatos();
+    } else {
+      console.error('Usuário não encontrado ou sem ID!');
+    }
+  }
+  
+  carregarCampeonatos(): void {
+    const campeonatos = JSON.parse(localStorage.getItem('campeonatos') || '[]');
+    this.campeonatos = campeonatos.filter((camp: Campeonato) => camp.userId === this.userId);
+  }
+  
+  logout() {
     this.authService.logout();
+  }
+
+  navegarcriarcamp() {
+    this.router.navigate(['/Criar_Campeonato']);
+  }
+
+  visuzalizarcamp(campeonatoId: string) {
+    this.router.navigate([`/Visualizar_Campeonato/${campeonatoId}`]);
   }
 }
